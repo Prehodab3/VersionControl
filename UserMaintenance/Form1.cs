ca005de884175942879429dc2400a9881b0b1248
Barna Prehoda <prehodab3@gmail.com>
2019-11-30 17:59:04 +0100

    5th commit

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,10 +18,33 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.FullName; // label1
-            label2.Text = Resource1.Add; // label2
-          
+            label1.Text = Resource1.FullName; 
             button1.Click += Button1_Click;
+            button2.Click += Button2_Click;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.txt)|*.txt";
+            sfd.DefaultExt = "txt";
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var u in users)
+                {
+                    sw.Write(u.FullName);
+                    sw.Write(";");
+                    sw.Write(value: u.ID);
+                    sw.WriteLine();
+                }
+            }
         }
 
         private void Button1_Click(object sender, EventArgs e)
